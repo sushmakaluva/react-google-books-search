8be21f8deb6e145171a8cfba2612be0db067fc4b
Sushma Kaluva <kaluvasushma@gmail.com>
2020-11-10 22:03:38 -0500

    added build folder

---
+++ client/build/precache-manifest.af6ca9b1290519fe757e181c7f3524ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8607a6a4517d98190f9a72ec59107e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08ad0acad21e0d0f614",
+    "url": "/static/css/main.7608dceb.chunk.css"
+  },
+  {
+    "revision": "3552cfe94f4361c22bbf",
+    "url": "/static/js/2.1d85c2ac.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.1d85c2ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08ad0acad21e0d0f614",
+    "url": "/static/js/main.e58d38ae.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
